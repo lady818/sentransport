c1ac3f51b7ac5723b09eafffce8dd6ed103b8f01
lady818 <mamesadioguisse@gmail.com>
2026-04-24 18:28:39 +0000

    Lab 2 : composants statiques LigneBus et ListeLignes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders app header and bus lines title", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(
+    screen.getByRole("heading", { name: /SenTransport/i, level: 1 })
+  ).toBeInTheDocument();
+  expect(
+    screen.getByRole("heading", { name: /Lignes Dakar Dem Dikk/i, level: 2 })
+  ).toBeInTheDocument();
 });
---
+++ src/Statistique.js
@@ -0,0 +1,12 @@
+import './Statistique.css';
+
+function Statistique() {
+  return (
+    <div className="stat">
+      <h2>10</h2>
+      <p>Lignes</p>
+    </div>
+  );
+}
+
+export default Statistique;
